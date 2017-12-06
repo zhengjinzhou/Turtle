2891093d48ace98753fc2b796909651b3b80e2a3
zhengjinzhou <1457594845@qq.com>
2017-12-06 15:55:34 +0800

    成功实现注册功能的mvp模式

--- app/src/main/java/com/zhou/turtle/view/LoginActivity.java
+++ app/src/main/java/com/zhou/turtle/view/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.zhou.turtle.view;
 
+import android.text.InputType;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
@@ -18,10 +19,9 @@ import butterknife.OnClick;
 public class LoginActivity extends BaseActivity implements ILogin {
 
     private static final String TAG = "LoginActivity";
-    @BindView(R.id.et_username)
-    EditText et_username;
-    @BindView(R.id.et_password)
-    EditText et_password;
+    @BindView(R.id.et_username) EditText et_username;
+    @BindView(R.id.et_password) EditText et_password;
+
     private LoginPresenter presenter = new LoginPresenter(this);
 
     @Override
@@ -34,11 +34,17 @@ public class LoginActivity extends BaseActivity implements ILogin {
 
     }
 
-    @OnClick({R.id.bt_login})
+    @OnClick({R.id.bt_login, R.id.clear, R.id.hint})
     void onClick(View view) {
         switch (view.getId()) {
             case R.id.bt_login:
                 presenter.login();
+                break;
+            case R.id.clear:
+                presenter.clear();
+                break;
+            case R.id.hint:
+                presenter.showPassword();
                 break;
         }
     }
@@ -63,17 +69,18 @@ public class LoginActivity extends BaseActivity implements ILogin {
 
     @Override
     public void toMainActivity(UserBean user) {
-        if (user.getError() == 0) {
-            startToActivity(MainActivity.class);
-            finish();
-        } else {
-            ToastUtil.show(getApplicationContext(), "用户名或者密码错误");
-        }
+        startToActivity(MainActivity.class);
+        finish();
     }
 
     @Override
     public void showFailedError() {
         ToastUtil.show(getApplicationContext(), "失败");
+    }
+
+    @Override
+    public void showLoginError() {
+        ToastUtil.show(getApplicationContext(), "用户名或者密码错误");
     }
 
     @Override
@@ -88,11 +95,18 @@ public class LoginActivity extends BaseActivity implements ILogin {
 
     @Override
     public void clearUserName() {
-
+        et_username.setText("");
     }
 
     @Override
     public void showPassword() {
-
+        int type = InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD;
+        if (et_password.getInputType() == type) {
+            et_password.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+            et_password.setSelection(et_password.getText().length());     //把光标设置到当前文本末尾
+        } else {
+            et_password.setInputType(type);
+            et_password.setSelection(et_password.getText().length());
+        }
     }
 }
--- app/src/main/java/com/zhou/turtle/view/RegisterActivity.java
+++ app/src/main/java/com/zhou/turtle/view/RegisterActivity.java
@@ -1,15 +1,107 @@
 package com.zhou.turtle.view;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.text.InputType;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
 
 import com.zhou.turtle.R;
+import com.zhou.turtle.base.BaseActivity;
+import com.zhou.turtle.model.bean.UserBean;
+import com.zhou.turtle.presenter.RegisterPrester;
+import com.zhou.turtle.util.ToastUtil;
+import com.zhou.turtle.view.IView.IRegister;
 
-public class RegisterActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.OnClick;
+
+public class RegisterActivity extends BaseActivity implements IRegister{
+
+    @BindView(R.id.et_username) EditText et_username;
+    @BindView(R.id.et_password) EditText et_password;
+    RegisterPrester prester = new RegisterPrester(this);
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+    public int getLayout() {
+        return R.layout.activity_register;
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @OnClick({R.id.bt_register,R.id.hint,R.id.clear}) void onClick(View view){
+        switch (view.getId()){
+            case R.id.bt_register:
+                prester.register();
+                break;
+            case R.id.hint:
+                prester.showPassowrd();
+                break;
+            case R.id.clear:
+                prester.clear();
+                break;
+        }
+    }
+    @Override
+    public String getUserName() {
+        if (TextUtils.isEmpty(et_username.getText().toString())){
+            ToastUtil.show(getApplicationContext(),"用户名不能为空");
+            return "";
+        }
+        return et_username.getText().toString();
+    }
+
+    @Override
+    public String getPassword() {
+        if (TextUtils.isEmpty(et_username.getText().toString())){
+            ToastUtil.show(getApplicationContext(),"密码不能为空");
+            return "";
+        }
+        return et_password.getText().toString();
+    }
+
+    @Override
+    public void toMainActivity(UserBean user) {
+        startToActivity(MainActivity.class);
+        finish();
+    }
+
+    @Override
+    public void showFailedError() {
+
+    }
+
+    @Override
+    public void showRegisterError() {
+        ToastUtil.show(getApplicationContext(),"用户已存在");
+    }
+
+    @Override
+    public void showLoading() {
+        dialog.show();
+    }
+
+    @Override
+    public void hideLoading() {
+        dialog.dismiss();
+    }
+
+    @Override
+    public void clearUserName() {
+        et_password.setText("");
+    }
+
+    @Override
+    public void showPassword() {
+        int type = InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD;
+        if (et_password.getInputType() == type) {
+            et_password.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+            et_password.setSelection(et_password.getText().length());     //把光标设置到当前文本末尾
+        } else {
+            et_password.setInputType(type);
+            et_password.setSelection(et_password.getText().length());
+        }
     }
 }
