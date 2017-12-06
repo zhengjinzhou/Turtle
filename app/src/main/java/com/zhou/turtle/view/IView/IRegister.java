fc10254ea0a5d7ddf49c6fba8c6aa4ebf347f76b
zhengjinzhou <1457594845@qq.com>
2017-12-06 15:54:07 +0800

    新增注册逻辑接口

---
+++ app/src/main/java/com/zhou/turtle/view/IView/IRegister.java
@@ -0,0 +1,28 @@
+package com.zhou.turtle.view.IView;
+
+import com.zhou.turtle.model.bean.UserBean;
+
+/**
+ * Created by zhou on 2017/12/6.
+ */
+
+public interface IRegister {
+
+    String getUserName();
+
+    String getPassword();
+
+    void toMainActivity(UserBean user);
+
+    void showFailedError();
+
+    void showRegisterError();
+
+    void showLoading();
+
+    void hideLoading();
+
+    void clearUserName();
+
+    void showPassword();
+}
